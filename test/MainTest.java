5d691e21a3beeb5739fc394a4b4e5126a9b66781
Jigar Gosar <jigar.gosar@gmail.com>
2017-08-29 17:08:59 +0530

    fix: jenkins build failing

--- test/MainTest.java
+++ test/MainTest.java
@@ -2,7 +2,7 @@ public class MainTest {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.printf("Counter! %d", 4);
+        System.out.printf("Counter! %d", 5);
     }
 }
 
