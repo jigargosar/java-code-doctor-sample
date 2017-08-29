4a7b6f3d2b2b5718fd1abb2f65eeee03fdb1dc43
Jigar Gosar <jigar.gosar@gmail.com>
2017-08-29 16:29:13 +0530

    fix: jenkins build failing

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.printf("Counter! %d", 2);
+        System.out.printf("Counter! %d", 3);
     }
 }
 
