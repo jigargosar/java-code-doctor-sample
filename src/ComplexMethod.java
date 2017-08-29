9008e9399cee1b93dc0cd3028950c86c810b15c3
Jigar Gosar <jigar.gosar@gmail.com>
2017-08-29 19:02:03 +0530

    fix: add sonar props

--- src/ComplexMethod.java
+++ src/ComplexMethod.java
@@ -26,4 +26,31 @@ public class ComplexMethod {
         return null;
     }
 
+
+    public String intToEnglishValueasdasda(int i) {
+        /*dfg dfgdsf gdsfg dsfg sdf gdsgds*/
+        if (i == 1) {
+            return "One";
+        }
+        if (i == 2) {
+            return "Two";
+        }
+        if (i == 3) {
+            return "Three";
+        }
+        if (i == 4) {
+            return "Four";
+        }
+        if (i == 5) {
+            return "Five";
+        }
+        if (i == 6) {
+            return "Six";
+        }
+        if (i > 6) {
+            throw new IllegalArgumentException(String.format("Invalid input i=%d", i));
+        }
+        return null;
+    }
+
 }
