d85026b5ebbd477d5ea885d4a066261299e77eab
Jigar Gosar <jigar.gosar@gmail.com>
2017-08-29 18:44:55 +0530

    fix: add sonar props

--- test/MainTest.java
+++ test/MainTest.java
@@ -2,7 +2,7 @@ public class MainTest {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.printf("Counter! %d", 5);
+        System.out.printf("Counter! %d", 6);
     }
 }
 
