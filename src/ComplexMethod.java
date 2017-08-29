7432696451b2d240c91a47fa5c0cacd6c4e0fc21
Jigar Gosar <jigar.gosar@gmail.com>
2017-08-29 14:57:17 +0530

    init

---
+++ src/ComplexMethod.java
@@ -0,0 +1,30 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class ComplexMethod {
+
+    public String intToEnglishValue(int i) {
+        if (i == 1) {
+            return "One";
+        }
+        if (i == 2) {
+            return "Two";
+        }
+        if (i == 3) {
+            return "Three";
+        }
+        if (i == 4) {
+            return "Four";
+        }
+        if (i == 5) {
+            return "Five";
+        }
+        if (i == 6) {
+            return "Six";
+        }
+        if (i > 6) {
+            throw new NotImplementedException();
+        }
+        return null;
+    }
+
+}
