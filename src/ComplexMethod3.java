e952d0555240f1861a6c82b7b9339df33fed4652
Jigar Gosar <jigar.gosar@gmail.com>
2017-09-02 17:46:15 +0530

    fix: add more duplicate code

---
+++ src/ComplexMethod3.java
@@ -0,0 +1,163 @@
+public class ComplexMethod3 {
+
+    public String iasdntToEnglishValue(int i) {
+        /*dfg dfgdsf gdsfg dsfg sdf gdsgds*/
+        if (i == 1) {
+            return "One";
+        }
+        if (i == 2) {
+            return "Two";
+        }
+        if (i == 3) {
+            return "Three";
+        }
+        if (i == 4) {
+            return "Four";
+        }
+        if (i == 5) {
+            return "Five";
+        }
+        if (i == 6) {
+            return "Six";
+        }
+        if (i > 6) {
+            throw new IllegalArgumentException(String.format("Invalid input i=%d", i));
+        }
+        return null;
+    }
+
+
+    public String intToEnsdfglishValueasdasda(int i) {
+        /*dfg dfgdsf gdsfg dsfg sdf gdsgds*/
+        if (i == 1) {
+            return "One";
+        }
+        if (i == 2) {
+            return "Two";
+        }
+        if (i == 3) {
+            return "Three";
+        }
+        if (i == 4) {
+            return "Four";
+        }
+        if (i == 5) {
+            return "Five";
+        }
+        if (i == 6) {
+            return "Six";
+        }
+        if (i > 6) {
+            throw new IllegalArgumentException(String.format("Invalid input i=%d", i));
+        }
+        return null;
+    }
+
+
+    public String intTsfdoEnglishValue(int i) {
+        /*dfg dfgdsf gdsfg dsfg sdf gdsgds*/
+        if (i == 1) {
+            return "One";
+        }
+        if (i == 2) {
+            return "Two";
+        }
+        if (i == 3) {
+            return "Three";
+        }
+        if (i == 4) {
+            return "Four";
+        }
+        if (i == 5) {
+            return "Five";
+        }
+        if (i == 6) {
+            return "Six";
+        }
+        if (i > 6) {
+            throw new IllegalArgumentException(String.format("Invalid input i=%d", i));
+        }
+        return null;
+    }
+
+
+    public String intToEnglish345Valueasdasda(int i) {
+        /*dfg dfgdsf gdsfg dsfg sdf gdsgds*/
+        if (i == 1) {
+            return "One";
+        }
+        if (i == 2) {
+            return "Two";
+        }
+        if (i == 3) {
+            return "Three";
+        }
+        if (i == 4) {
+            return "Four";
+        }
+        if (i == 5) {
+            return "Five";
+        }
+        if (i == 6) {
+            return "Six";
+        }
+        if (i > 6) {
+            throw new IllegalArgumentException(String.format("Invalid input i=%d", i));
+        }
+        return null;
+    }
+
+    public String intToEnglishValue(int i) {
+        /*dfg dfgdsf gdsfg dsfg sdf gdsgds*/
+        if (i == 1) {
+            return "One";
+        }
+        if (i == 2) {
+            return "Two";
+        }
+        if (i == 3) {
+            return "Three";
+        }
+        if (i == 4) {
+            return "Four";
+        }
+        if (i == 5) {
+            return "Five";
+        }
+        if (i == 6) {
+            return "Six";
+        }
+        if (i > 6) {
+            throw new IllegalArgumentException(String.format("Invalid input i=%d", i));
+        }
+        return null;
+    }
+
+
+    public String intToEnglishValueasdasda(int i) {
+        /*dfg dfgdsf gdsfg dsfg sdf gdsgds*/
+        if (i == 1) {
+            return "One";
+        }
+        if (i == 2) {
+            return "Two";
+        }
+        if (i == 3) {
+            return "Three";
+        }
+        if (i == 4) {
+            return "Four";
+        }
+        if (i == 5) {
+            return "Five";
+        }
+        if (i == 6) {
+            return "Six";
+        }
+        if (i > 6) {
+            throw new IllegalArgumentException(String.format("Invalid input i=%d", i));
+        }
+        return null;
+    }
+
+}
