f5d11224e01c9432a1e77f17fee3fd3d532d7909
Jigar Gosar <jigar.gosar@gmail.com>
2017-09-03 17:58:42 +0530

    adding maven/test support

--- src/main/java/com/agilefaq/codedoctorsample/App.java
+++ src/main/java/com/agilefaq/codedoctorsample/App.java
@@ -7,4 +7,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static int calc(int a1, int a2) {
+        return a1 + a2;
+    }
 }
