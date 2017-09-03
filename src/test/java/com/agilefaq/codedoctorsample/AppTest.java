9a62564c8cf57064dc69b2ded0f110ee612192d6
Jigar Gosar <jigar.gosar@gmail.com>
2017-09-03 18:49:23 +0530

    adding maven/test support

--- src/test/java/com/agilefaq/codedoctorsample/AppTest.java
+++ src/test/java/com/agilefaq/codedoctorsample/AppTest.java
@@ -1,37 +1,12 @@
 package com.agilefaq.codedoctorsample;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
+import static org.junit.Assert.assertEquals;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
+public class AppTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
-    }
-
+    @Test
     public void testCalc() throws Exception {
         int res = App.calc(1, 1);
         assertEquals(2, res);
