504a3e77965200f48cd6d7180cce3b80ab1e01ab
Jigar Gosar <jigar.gosar@gmail.com>
2017-09-05 13:39:37 +0530

    fixes #10

--- src/main/java/com/agilefaq/codedoctorsample/App.java
+++ src/main/java/com/agilefaq/codedoctorsample/App.java
@@ -16,14 +16,6 @@ public class App {
         return a1 * a2;
     }
 
-    public static int multiply2(int a1, int a2) {
-        return a1 * a2;
-    }
-
-    public static int multiply3(int a1, int a2) {
-        return a1 * a2;
-    }
-
     public static int multiply4(int a1, int a2) {
         if (a1 == 1) {
             a1 = 2;
