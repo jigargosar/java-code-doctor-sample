780604afbe876cf8aab237a43213b798666e047f
Jigar Gosar <jigar.gosar@gmail.com>
2017-09-05 13:48:19 +0530

    introducing duplication

--- src/main/java/com/agilefaq/codedoctorsample/App.java
+++ src/main/java/com/agilefaq/codedoctorsample/App.java
@@ -13,16 +13,17 @@ public class App {
     }
 
     public static int multiply(int a1, int a2) {
+        Point point = new Point();
+        Point point1 = new Point();
+        if (point1 == point) {
+            System.out.println("Equal");
+        }
         return a1 * a2;
     }
 
     public static int multiply4(int a1, int a2) {
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        if (a1 == 1) {
-            a1 = 2;
-        }
+        String a = null;
+        System.out.println(a.charAt(0));
         if (a1 == 1) {
             a1 = 2;
         }
@@ -39,5 +40,25 @@ public class App {
             a1 = 2;
         }
         return a1 * a2;
+    }
+}
+
+
+class Point {
+    private int x = 0;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Point point = (Point) o;
+
+        return x == point.x;
+    }
+
+    @Override
+    public int hashCode() {
+        return x;
     }
 }
