534620d607058513b4602ad0e927fe6c0899e610
Jigar Gosar <jigar.gosar@gmail.com>
2017-09-05 13:29:52 +0530

    fixing test
    fixes #1

--- src/test/java/com/agilefaq/codedoctorsample/AppTest.java
+++ src/test/java/com/agilefaq/codedoctorsample/AppTest.java
@@ -14,8 +14,8 @@ public class AppTest {
 
     @Test
     public void testCalc2() throws Exception {
-        int res = App.calc(1, 1);
-        assertEquals(2, res);
+        int res = App.calc2(1, 1);
+        assertEquals(0, res);
     }
 
     @Test
