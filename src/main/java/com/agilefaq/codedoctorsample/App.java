58d928cb99454436969e76f893365e775a69d9e2
Jigar Gosar <jigar.gosar@gmail.com>
2017-09-19 18:41:50 +0530

    test local gitlab build

--- src/main/java/com/agilefaq/codedoctorsample/App.java
+++ src/main/java/com/agilefaq/codedoctorsample/App.java
@@ -1,9 +1,5 @@
 package com.agilefaq.codedoctorsample;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 /**
  * Hello world!
  */
@@ -55,58 +51,6 @@ public class App {
         }
         return a1 * a2;
     }
-
-
-    public static int multiply4(int a1, int a2) {
-
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
-                    "user=steve&password=blue");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        java.net.PasswordAuthentication pa = new java.net.PasswordAuthentication("userName", "1234".toCharArray());
-
-
-        String a = null;
-        System.out.println(a.charAt(0));
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        if (a1 == 1) {
-            a1 = 2;
-        }
-        return a1 * a2;
-    }
-
 }
 
 
